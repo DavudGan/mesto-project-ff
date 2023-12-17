26aefeafacc9e7cfe414657f511a1d4724d5c2a5
Davud Ganbarov <davud.ganbaroff@yandex.ru>
2023-12-17 09:48:09 +0500

    firste-commit_1

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
     {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      name: "Екатеринбург",
+      link: "https://vmrucdn.servicecdn.ru/photo/vecherka/2019/06/doc75tvju274memsdccjv2_800_480.jpg",
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      name: "Нео-Токио из «Акира»",
+      link: "https://avatars.dzeninfra.ru/get-zen_doc/230865/pub_5dcfc6e9042363115e30817c_5dd12c5ec7891f51f5fba7b2/orig",
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      name: "Корико из «Ведьмина служба доставки»",
+      link: "https://avatars.dzeninfra.ru/get-zen_doc/965902/pub_5dcfc6e9042363115e30817c_5dd130a308ffd321245c3f99/orig",
     },
     {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      name: "Арун из «Sword Art Online»",
+      link: "https://avatars.dzeninfra.ru/get-zen_doc/1537151/pub_5dcfc6e9042363115e30817c_5dd1443143ca9b53a97055cc/orig",
     },
     {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      name: "Магнолия из «Хвост феи»",
+      link: "https://avatars.dzeninfra.ru/get-zen_doc/1904927/pub_5dcfc6e9042363115e30817c_5dd146b9eec9e2085b07a9c3/orig",
     },
     {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      name: "Орарио из «Может, я встречу тебя в подземелье?»",
+      link: "https://avatars.dzeninfra.ru/get-zen_doc/1922981/pub_5dcfc6e9042363115e30817c_5dd148f0f3abe118dc3ad147/scale_1200",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
-// @todo: Темплейт карточки
+// // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// // @todo: DOM узлы
+const places = document.querySelector('.places__list');
+const deleteElement = document.querySelector('.card__delete-button');
 
 // @todo: Функция создания карточки
+function addCard (titleValue, imageValue) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__image').src = imageValue;
+    cardElement.querySelector('.card__title').textContent = titleValue;
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', function(){deleteCard(cardElement)});
+    
+    places.append(cardElement);
+};
 
 // @todo: Функция удаления карточки
+function deleteCard (cardEl) {
+    const deleteButton = cardEl.querySelector('.card__delete-button');
+    const cardToRemove = deleteButton.parentElement;
+    cardToRemove.remove();
+};
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item, index) => {
+    addCard(initialCards[index].name, initialCards[index].link);
+});
+
